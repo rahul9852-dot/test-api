66f5baecf42f256c7b8d6d54bec51263e508d766
admin <admin@admins-MacBook-Pro.local>
2021-06-13 20:25:03 +0530

    add server

---
+++ server.js
@@ -0,0 +1,11 @@
+const jsonserver=require('json-server');
+const server=jsonserver.create();
+const router=jsonserver.router('db.json');
+const middleware=jsonserver.defaults();
+const port=process.env.PORT||3000;
+
+
+server.use(middleware);
+server.use(router);
+
+server.listen(port);
